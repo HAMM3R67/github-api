b57ddbdd5cf99094f05b9d8bbf01ff56cbb8d4ac
HAMM3R67 <tcschauer@gmail.com>
2015-08-18 18:56:35 -0600

    added app to controller and service

---
+++ controller.js
@@ -0,0 +1,4 @@
+var app = angular.module("github-api")
+.controller("apiController", function($scope){
+	
+})
---
+++ service.js
@@ -0,0 +1,8 @@
+var app = angular.module("github-api")
+.service("apiService", function(){
+	
+	var id = "e0dc749dae6bc7fba369";
+  var sec = "8f604f8817068c04e666b88b11f5c91c0afd9e76";
+  var param = "?client_id=" + id + "&client_secret=" + sec;
+	
+})
