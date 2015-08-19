b62bee0626d93ba8ba92f16fdf069bf9a329f5c2
HAMM3R67 <tcschauer@gmail.com>
2015-08-18 19:07:59 -0600

    added $scope.searchText as a parameter to getUsers

--- controller.js
+++ controller.js
@@ -2,7 +2,7 @@ var app = angular.module("github-api")
 .controller("GitHubController", function($scope, githubService){
 	
 	$scope.getUserData = function(){
-		githubService.getUser().then(result){
+		githubService.getUser($scope.searchText).then(result){
 			$scope.user = result.data
 		}
 	}
