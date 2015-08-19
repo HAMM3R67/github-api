b93df01997af84208841fa9a7aee3e66266652e8
HAMM3R67 <tcschauer@gmail.com>
2015-08-18 19:12:26 -0600

    fixed some syntax

--- controller.js
+++ controller.js
@@ -2,9 +2,9 @@ var app = angular.module("github-api")
 .controller("GitHubController", function($scope, githubService){
 	
 	$scope.getUserData = function(){
-		githubService.getUser($scope.searchText).then(result){
+		githubService.getUser($scope.searchText).then(result)
 			$scope.user = result.data
-		}
+		
 	}
 	
 })
