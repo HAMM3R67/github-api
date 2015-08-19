c88bccb47367ec9b14f92fba79219255fd47eff2
HAMM3R67 <tcschauer@gmail.com>
2015-08-18 19:07:13 -0600

    created $scope.getUserData set it to call getUser

--- controller.js
+++ controller.js
@@ -1,6 +1,10 @@
 var app = angular.module("github-api")
 .controller("GitHubController", function($scope, githubService){
 	
-	
+	$scope.getUserData = function(){
+		githubService.getUser().then(result){
+			$scope.user = result.data
+		}
+	}
 	
 })
