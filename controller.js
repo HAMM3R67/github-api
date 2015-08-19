e1d069691eab8eb720430aeb844606902df016c9
HAMM3R67 <tcschauer@gmail.com>
2015-08-18 19:48:06 -0600

    edited html to pull specific information from the input username

--- controller.js
+++ controller.js
@@ -4,7 +4,9 @@ app.controller('gitHubController', function($scope, githubService){
 	
 	$scope.getUserData = function(){
 		githubService.getUser($scope.searchText).then(function(result){
+			console.log(result);
 			$scope.user = result.data;
+			
 		})	
 	}
 	
